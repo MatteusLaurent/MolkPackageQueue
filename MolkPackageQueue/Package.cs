7145907b3bf022282e9ba050d2c6b91722490b40
MinnaDeVerdier <minna.deverdier@gmail.com>
2024-03-21 15:11:22 +0100

    Minna MJU23-1

--- MolkPackageQueue/Package.cs
+++ MolkPackageQueue/Package.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,15 +9,34 @@ namespace MolkPackageQueue
 {
     public class Package
     {
+        /// <summary>
+        /// Creates a package with the given priority, and a random name and ID
+        /// </summary>
+        /// <param name="priority"></param>
         public Package(Priority priority)
         {
             Priority = priority;
             Payload = new Payload();
         }
+        /// <summary>
+        /// Creates a Package with the given priority and name, and a random ID
+        /// </summary>
+        /// <param name="priority"></param>
+        /// <param name="packageName"></param>
+        public Package(Priority priority, string packageName)
+        {
+            Priority = priority;
+            Payload = new Payload(packageName);
+        }
         public Priority Priority { get; }
         public Payload Payload { get; }
     }
 
+    /// <summary>
+    /// <list type="number"> Low
+    /// <item/>Medium
+    /// <item/>High</list>
+    /// </summary>
     public enum Priority 
     { 
         Low = 0, 
@@ -24,8 +44,49 @@ namespace MolkPackageQueue
         High = 2 
     }
 
-    public class Payload 
+    /// <summary>
+    /// Handles the package name and ID 
+    /// </summary>
+    public class Payload
     {
-        string packageName = string.Empty; //Replace with a random name (string of letters) for each instance
+        public Guid PackageGuid { get; } = Guid.NewGuid();
+        public string PackageName { get; }
+        /// <summary>
+        /// Constructor with random package name
+        /// </summary>
+        public Payload()
+        {
+            PackageName = RandomPackageName();
+        }
+        /// <summary>
+        /// Constructor with given package name
+        /// </summary>
+        /// <param name="name">sender specified package name</param>
+        public Payload(string name)
+        {
+            PackageName = name;
+        }
+        /// <summary>
+        /// Creates a random package name
+        /// </summary>
+        /// <returns>String of 3-5 capitalized letters</returns>
+        public static string RandomPackageName()
+        {
+            Random rand = new Random();
+            StringBuilder builder = new(rand.Next(3,6));
+            for (var i = 0; i < builder.Capacity; i++)
+            {
+                char letter = (char)rand.Next('A', 'Z'+1);
+                builder.Append(letter);
+            }
+            return builder.ToString();
+        }
+  
+        /// <returns>String with package name and package ID</returns>
+        public override string ToString()
+        {
+            string contents = $"Package \"{PackageName}\"\tID: {PackageGuid}";
+            return contents;
+        }
     }
 }
--- MolkPackageQueue/PriorityQueue.cs
+++ MolkPackageQueue/PriorityQueue.cs
@@ -12,13 +12,67 @@ namespace MolkPackageQueue
         Queue<Package> queueMedium = new Queue<Package>();
         Queue<Package> queueLow = new Queue<Package>();
 
-        public void Enqueue(Package package)
+        public List<Package> incomingPackagesLog { get; } = new List<Package>();
+        public List<Package> outgoingPackagesLog { get; } = new List<Package>();
+
+        /// <summary>
+        /// Places the Package in the correct Priority queue and logs it as incoming
+        /// </summary>
+        /// <returns>Awaitable Task</returns>
+        /// <param name="package"></param>
+        /// <remarks>Any package not sent with a priority gets the lowest priority</remarks>
+        public async Task Enqueue(Package package)
         {
-            // do stuff
+            switch(package.Priority)
+            {
+                case Priority.High:
+                    queueHigh.Enqueue(package);
+                    break;
+                case Priority.Medium: 
+                    queueMedium.Enqueue(package);
+                    break;
+                default:                        
+                    queueLow.Enqueue(package);
+                    break;
+            }
+            incomingPackagesLog.Add(package);
         }
-        public void Dequeue(Package package)
+        /// <summary>
+        /// Removes one package from a queue by order of priority, and logs it as outgoing.
+        /// </summary>
+        /// <returns>Awaitable Task</returns>
+        /// <remarks>If the sent (outgoing) packages are as many as have been recieved (incoming), no packages are dequeued</remarks>
+        public async Task<TaskStatus> Dequeue()
         {
-            // do stuff
+            if (incomingPackagesLog.Count == outgoingPackagesLog.Count) 
+                return TaskStatus.Canceled;
+            if (queueHigh.Count > 0)
+            {
+                outgoingPackagesLog.Add(queueHigh.Dequeue());
+                return TaskStatus.RanToCompletion;
+            }
+            else if (queueMedium.Count > 0)
+            {
+                outgoingPackagesLog.Add(queueMedium.Dequeue());
+                return TaskStatus.RanToCompletion;
+            }
+            else if (queueLow.Count > 0)
+            {
+                outgoingPackagesLog.Add(queueLow.Dequeue());
+                return TaskStatus.RanToCompletion;
+            }
+            return TaskStatus.Canceled;
+        }
+
+        /// <summary>
+        /// Checks that all queues in this PriorityQueue are empty
+        /// </summary>
+        /// <returns>true if all queues are empty; false if there are packages left in any queue</returns>
+        public bool AreQueuesEmpty()
+        {
+            if (queueHigh.Count > 0 || queueMedium.Count > 0 || queueLow.Count > 0)
+                return false; 
+            return true;
         }
     }
 }
